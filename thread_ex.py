b3fedebeb082b01ff4d323c65260f23b625f6b23
ershan <ershan_coding@outlook.com>
2022-04-10 21:51:12 +0800

    Add pyton thread example.
    Signed-off-by: ershan <ershan_coding@outlook.com>

---
+++ thread_ex.py
@@ -0,0 +1,39 @@
+'''
+文件:   thread_ex.py
+作者:   二山
+邮箱:   ershan_coding@outlook.com
+描述:   Python 多线程的例子
+'''
+
+
+import time
+import threading
+
+import clipboard
+
+
+Flag = True
+
+
+def backend():
+    global Flag
+    while Flag:
+        text = clipboard.paste()
+        clipboard.copy(text.upper())
+        time.sleep(0.01)
+    print("exit")
+
+
+if __name__ == "__main__":
+    th = threading.Thread(target=backend)
+    th.start()
+
+    while True:
+        flag = input(">>> ")
+        if flag.casefold() == 'q':
+            Flag = False
+            break
+        elif flag.casefold() == 'print':
+            print("from main")
+        else:
+            print("unknown command")
